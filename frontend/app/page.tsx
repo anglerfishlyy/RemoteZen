38a7185cf94298e755efb5be39e26430867bb548
anglerfishlyy <kavyasamudralaA7@gmail.com>
2025-09-02 18:39:10 +0530

    basic-next.js+tailwindcss-code

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+     <div className="flex h-screen items-center justify-center bg-gray-100">
+      <button className="px-6 py-2 rounded-lg bg-blue-500 text-white hover:bg-blue-600">
+        Tailwind Test Button
+      </button>
+    </div>
+  );
+}
